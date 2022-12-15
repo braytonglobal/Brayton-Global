43170ac3983b98b76b853b2f723a14e1cda518fd
User 2 <user2@MacBook-Air-de-Christina.local>
2022-12-15 16:00:12 +0100

    lastUpdated and view more openings button : rss.js

--- js/rss.js
+++ js/rss.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
       autoplaySpeed: 3000,
       accessibility: true,
       draggable: true,
-      pauseOnDotsHover:true,
+      pauseOnDotsHover: true,
       swipe: true,
       zIndex: 1000,
       mobileFirst: true,
@@ -54,7 +54,7 @@ $(document).ready(() => {
   }
   sliderInit();
   /***************************************************************Openings DataTables Skeleton************************************************** */
-  let dataTableSkeleton =`<thead>
+  let dataTableSkeleton = `<thead>
   <tr>
     <th></th>
     <th><p class='skeleton skeleton-text'></p></th>
@@ -66,27 +66,27 @@ $(document).ready(() => {
   </tr>
  </thead>
 <tbody id="bd"></tbody>`
-$(dataTableSkeleton).appendTo(".itemTable:first-of-type")
-$(".itemTable:first-of-type").addClass('skeleton-table')
-for (let i = 0; i < 5; i++) {
-  $("<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>").appendTo('.itemTable:first-of-type #bd');
-}
-$("<p class='skeleton skeleton-text'></p>").appendTo('.itemTable:first-of-type #bd td');
-$('.itemTable:first-of-type #bd td:first-child p').removeClass('skeleton-text').addClass('skeleton-icon');
-var dt = $("#positionsTable").DataTable({
-  info: false,
-  lengthChange: false,
-  ordering: false,
- pagination:false,
- filter:false,
-  pageLength: 5,
-})
- /*_________________________________________________________________________________________________________________________________________________ */
+  $(dataTableSkeleton).appendTo(".itemTable:first-of-type")
+  $(".itemTable:first-of-type").addClass('skeleton-table')
+  for (let i = 0; i < 5; i++) {
+    $("<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>").appendTo('.itemTable:first-of-type #bd');
+  }
+  $("<p class='skeleton skeleton-text'></p>").appendTo('.itemTable:first-of-type #bd td');
+  $('.itemTable:first-of-type #bd td:first-child p').removeClass('skeleton-text').addClass('skeleton-icon');
+  var dt = $("#positionsTable").DataTable({
+    info: false,
+    lengthChange: false,
+    ordering: false,
+    pagination: false,
+    filter: false,
+    pageLength: 5,
+  })
+  /*_________________________________________________________________________________________________________________________________________________ */
   /*_____***************************************************************END SKELETON LOADER*************************************************** */
   /*_________________________________________________________________________________________________________________________________________________ */
 
 
-   /*---------------------------------------------------------------------------------------------------------------------------------------------*/
+  /*---------------------------------------------------------------------------------------------------------------------------------------------*/
   /*-----****************************************************************START XML HTTP REQUEST*************************************************** */
   /*---------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -95,7 +95,7 @@ var dt = $("#positionsTable").DataTable({
   request.responseType = 'document';
   request.overrideMimeType('text/xml');
 
-  function errorDisplay(){
+  function errorDisplay() {
     const message = "You can usually find our open positions here, but there was a problem connecting to the server.<br/> Please try again later"
     dt.destroy()
     $(".itemGroup").slick("unslick")
@@ -114,7 +114,7 @@ var dt = $("#positionsTable").DataTable({
   request.onload = function () {
     if (request.readyState === request.DONE) {
       //Case request error
-      if(request.status >= 400 && request.status < 600) errorDisplay();
+      if (request.status >= 400 && request.status < 600) errorDisplay();
       //Case request OK
       else if (request.status === 200) {
         dt.destroy()
@@ -172,7 +172,7 @@ var dt = $("#positionsTable").DataTable({
        %0D%0AHere are my coordinates: (Please fill your coordinates)
        %0D%0A
        %0D%0AKind regards,`
-              var button = `<a class="pbtn positionBtn" href="mailto:${emailAddress}?subject=${reference} ${position}&body=${mailBody}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at-fill" viewBox="0 0 16 16">
+              var button = `<a class="positionBtn" href="mailto:${emailAddress}?subject=${reference} ${position}&body=${mailBody}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at-fill" viewBox="0 0 16 16">
        <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z"/>
        <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034v.21Zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z"/>
      </svg> Apply Now</a>`
@@ -214,10 +214,8 @@ var dt = $("#positionsTable").DataTable({
           } else {
             $("<div class='alert alert-dark' role='alert'>No job opening recommendations .Click on the button below to view published jobs</div>").appendTo(".itemGroup:first-of-type")
           }
-          $('.itemGroup:first-of-type').after('<a class="btn" href="#openings">View full openings list</a>')
+          $('.itemGroup:first-of-type').after('<a class="positionBtn" style="background-color:#0b1014;width:200px;text-align:center" href="#openings">View full openings list</a>')
           //End Case there are no oepnings to promote
-
-
 
           /************************************promoted and published jobs table with DataTables plugin:OPENINGS*****************************************************************************************************************/
 
@@ -285,7 +283,6 @@ var dt = $("#positionsTable").DataTable({
           });
           var footnote = itemTable[itemTable.length - 1].getElementsByTagName('footnote')[0].innerHTML
           $('.itemTable:first-of-type').after("<p>" + footnote + "</p>")
-          $('.itemTable:first-of-type').after(`<p>Last updated on ${updateDate}`);
           $("#positionsTable").DataTable({
             info: true,
             lengthChange: false,
@@ -308,7 +305,8 @@ var dt = $("#positionsTable").DataTable({
             order: [[0, 'desc']],
 
           })
-          $('.dataTables_wrapper .dataTables_length select option').append(' per page')
+          /*$('.dataTables_wrapper .dataTables_length select option').append(' per page')*/
+          $('#positionsTable_wrapper').prepend(`<div class=" float-start fw-bold fst-italic">Last updated on ${updateDate}</div>`);
         } else {
           $("<div class='alert alert-warning' role='alert'>No openings available . Please try another time</div>").appendTo(".itemGroup:first-of-type")
           $("<div class='alert alert-warning' role='alert'>No openings availables . Please try another time</div>").appendTo(".itemTable:first-of-type")
